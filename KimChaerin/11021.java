a3db09cafe9c0596e902db6b01c3cd0bde64da55
chaerin <90518654+chaerin-off@users.noreply.github.com>
2022-06-22 23:47:10 +0900

    [브론즈3] A+B - 7

---
+++ KimChaerin/11021.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+    	int T = sc.nextInt();
+    	for(int i=0;i<T;i++) {
+    		int A = sc.nextInt();
+    		int B = sc.nextInt();
+    		System.out.println("Case #"+(i+1)+": "+(A+B)); //정수를 더할때는 ()로 우선순위 정한 뒤 출력하기
+    	}
+    }
+}
